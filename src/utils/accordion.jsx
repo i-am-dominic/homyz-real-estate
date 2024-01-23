4eaf73c76c97bda0aa27843abcba70cd386f1cc2
Dominic Iyasele <dominiciyasele@gmail.com>
2024-01-23 14:29:11 +0100

    Initial commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, minima.",
+  },
+];
+export default data;
